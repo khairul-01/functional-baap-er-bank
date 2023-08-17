1ad2d9bdd29fba0f6863d98c0fa5f47d069a6c60
khairul-01 <khairul431743@gmail.com>
2023-08-17 12:52:03 +0600

    deposit button is interactivated with javascript

--- js/deposit.js
+++ js/deposit.js
@@ -1,4 +1,16 @@
 
 document.getElementById('btn-deposit').addEventListener('click', function() {
-   console.log('deposit button is clicked');
+   // Getting input value.
+   const newDepositAmount = getInputValueById('deposit-input');
+   // getting previous deposit total.
+   const previousDepositTotal = getElementValueById('deposit-total');
+   // setting new deposit value.
+   const newDepositTotal = previousDepositTotal + newDepositAmount;
+   setTextValueById('deposit-total', newDepositTotal);
+
+   // getting previous balance 
+   const previousBalance = getElementValueById('balance');
+   const newBalance = previousBalance + newDepositAmount;
+   // setting new balance
+   setTextValueById('balance', newBalance);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,20 @@
-const myutilities = 'hello, variable inside utilities file.'
 
-console.log('This is utilities files.');
+function getInputValueById(inputId) {
+   const inputField = document.getElementById(inputId);
+   const inputFieldValueString = inputField.value;
+   const inputFieldValue = parseFloat(inputFieldValueString);
+   inputField.value = '';
+   return inputFieldValue;
+}
+
+function getElementValueById(elementId) {
+   const element = document.getElementById(elementId);
+   const elementValueString = element.innerText;
+   const elementValue = parseFloat(elementValueString);
+   return elementValue;
+}
+
+function setTextValueById(elementId, newValue){
+   const textElement = document.getElementById(elementId);
+   textElement.innerText = newValue;
+}
